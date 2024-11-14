2349e6135f6be92da510791e9540553f0c62547d
Sentinal-cell <ahmadajiya12@gmail.com>
2024-11-14 23:14:51 +0100

    w

--- src/main/java/com/auto/Main.java
+++ src/main/java/com/auto/Main.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class WebAutomationExample {
+public class Main {
     private static final String url = "jdbc:mysql://localhost:3306/scrape";
     private static final String usr = "root";
     private static final String pass = "root";
